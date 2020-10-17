5da71199e5507e03d40f733b67134e60f508e80b
Abbashozefa <72591326+Abbashozefa@users.noreply.github.com>
2020-10-17 16:14:45 +0530

    Update EYE HOSPITAL DATABASE.py

--- EYE HOSPITAL DATABASE.py
+++ EYE HOSPITAL DATABASE.py
@@ -7,9 +7,10 @@ a=conn.cursor()
 print('1.RECEPTIONIST')
 print('2.OPTOMETRIST(GENERAL CHECKUP)')
 print('3.OPTHALMOLOGIST(DOCTOR)')
-ch1=int(input('SELECT YOUR FIELD'))
+ch1=int(input('SELECT YOUR FIELD:::'))
 
 def receptionist():
+    
     conn=pymysql.connect(host='localhost',user='root',password='yellowyellow',database='EYEHOSPITAL')
     a=conn.cursor()
 
@@ -17,7 +18,7 @@ def receptionist():
     print('1.NEW PATIENT')
     print('2.ROUTINE CHECKUP PATIENT DETAILS')
     print('3.VIEW PATIENT DETAILS')
-    ch2=int(input('SELECT YOUR FIELD'))
+    ch2=int(input('SELECT YOUR FIELD:::'))
    
     
     '''a.execute('CREATE TABLE PATIENT(PATIENT_ID INT PRIMARY KEY ,PATIENT_NAME VARCHAR(20),AGE INT,DATE_OF_LAST_VISIT DATE,CONSULTING_DOCTOR VARCHAR(20),RIGHT_SPH DECIMAL(2,1),RIGHT_CYL DECIMAL(2,1),RIGHT_AXIS INT,LEFT_SPH DECIMAL(2,1),LEFT_CYL DECIMAL(2,1),LEFT_AXIS INT)')'''
@@ -27,11 +28,11 @@ def receptionist():
         
         
 
-        user=int(input('Enter PATIENT ID'))
-        name=input('Enter PATIENT NAME')
-        age=int(input('Enter PATIENT AGE'))
-        date=input('Enter DATE')
-        doc=input('Enter CONSULTING DOCTOR')
+        user=int(input('Enter PATIENT ID:'))
+        name=input('Enter PATIENT NAME:')
+        age=int(input('Enter PATIENT AGE:'))
+        date=input('Enter DATE:')
+        doc=input('Enter CONSULTING DOCTOR:')
          
          
 
@@ -50,7 +51,20 @@ def receptionist():
         
         print(data.iloc[:,0:5])
         
-
+    elif(ch2==2):
+        user=int(input('Enter PATIENT ID'))
+        name=input('Enter PATIENT NAME')
+        a.execute("select PATIENT_ID  ,PATIENT_NAME,AGE,DATE_OF_LAST_VISIT,CONSULTING_DOCTOR from PATIENT where PATIENT_ID="+str(user)+" and PATIENT_NAME='"+name+"'" )
+        D=a.fetchall()
+        data=pd.DataFrame(D,columns=['PATIENT_ID ' ,'PATIENT_NAME','AGE','DATE_OF_LAST_VISIT','CONSULTING_DOCTOR'])
+        data.set_index('PATIENT_ID ',inplace=True)
+        print(data.iloc[:,0:5])
+        a.execute("select *from PATIENT")
+        D=a.fetchall()
+        data=pd.DataFrame(D,columns=['PATIENT_ID ' ,'PATIENT_NAME','AGE','DATE_OF_LAST_VISIT','CONSULTING_DOCTOR','RIGHT_SPH','RIGHT_CYL','RIGHT_AXIS','LEFT_SPH','LEFT_CYL','LEFT_AXIS'])
+        
+        print(data.iloc[:,0:5])
+        
     conn.commit()
 
 if(ch1==1):
